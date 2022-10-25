d528ff0eb40234a06e77c2e8a7f4e620256f8def
joshl26 <joshl26@hotmail.com>
2022-10-25 13:53:53 -0400

    commit

---
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -0,0 +1,12 @@
+import classes from "./MealItemForm.module.css";
+
+const MealItemForm = (props) => {
+  return (
+    <form className={classes.meal}>
+      <input />
+      <button>+ Add</button>
+    </form>
+  );
+};
+
+export default MealItemForm;
